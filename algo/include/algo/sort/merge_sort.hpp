e96aee9d09e925cd7fe05462a22e91826a3537f3
Todd W. Fulton <todd.w.fulton@gmail.com>
2023-10-17 09:39:26 -0500

    fix: clang analyzer/tidy crashes
    Clang analyzer/tidy crashes on analyzing lambdas using
    deducing this.

--- algo/include/algo/sort/merge_sort.hpp
+++ algo/include/algo/sort/merge_sort.hpp
@@ -11,56 +11,75 @@ void merge_sort(RNG& rng, CMP cmp = CMP{})
 {
 
     // TODO: Convert to loop based algorithm.
-    
-    auto merge = [&cmp](auto dest, auto litr, auto lend, auto ritr, auto rend) {
+    auto size = std::size(rng);
+    // NOTE: (A.1) This is too limiting or just wrong, not limiting enough.
+    if (size >
+        std::numeric_limits<std::ranges::range_difference_t<RNG>>::max()) {
+        // TODO: use proper exception.
+        throw std::exception();
+    }
 
-        while(litr != lend and ritr != rend) {
-            if(cmp(*litr, *ritr)) {
-                *dest = std::move(*litr);
-                ++litr;
-            } else {
-                *dest = std::move(*ritr);
-                ++ritr;
+    auto merge =
+        [&cmp](auto dest, auto litr, auto lend, auto ritr, auto rend) {
+            while (litr != lend and ritr != rend) {
+                if (cmp(*litr, *ritr)) {
+                    *dest = std::move(*litr);
+                    ++litr;
+                }
+                else {
+                    *dest = std::move(*ritr);
+                    ++ritr;
+                }
+                ++dest;
             }
-            ++dest;
-        }
 
-        while(litr != lend) {
-            *dest = std::move(*litr);
-            ++dest, ++litr;
-        }
+            while (litr != lend) {
+                *dest = std::move(*litr);
+                ++dest, ++litr;
+            }
 
-        while(ritr != rend) {
-            *dest = std::move(*ritr);
-            ++dest, ++ritr;
-        }
-    };
+            while (ritr != rend) {
+                *dest = std::move(*ritr);
+                ++dest, ++ritr;
+            }
+        };
 
-    auto top_down_split_merge = [&merge](this auto&& self, auto src, auto dst, long size) {
-        if (size <= 1) { return ;}
-        
-        auto sz_left = size / 2 + (size % 2);
-        auto sz_right = size / 2;
+    auto top_down_split_merge =
+        [&merge](auto const& self,
+                 auto src,
+                 auto dst,
+                 std::ranges::range_difference_t<RNG> size) {
+            if (size <= 1) {
+                return;
+            }
 
-        auto sbegin_left{src};
-        auto sbegin_right{src + sz_left};
+            auto sz_left = size / 2 + (size % 2);
+            auto sz_right = size / 2;
 
-        auto dbegin_left{dst};
-        auto dend_left{dst + sz_left};
-        auto dbegin_right{dst + sz_left};
-        auto dend_right{dst + size};
+            auto sbegin_left{src};
+            auto sbegin_right{src + sz_left};
 
+            auto dbegin_left{dst};
+            auto dend_left{dst + sz_left};
+            auto dbegin_right{dst + sz_left};
+            auto dend_right{dst + size};
 
-        self(dbegin_left, sbegin_left, sz_left);
-        self(dbegin_right, sbegin_right, sz_right);
-        merge(src, dbegin_left, dend_left, dbegin_right, dend_right);
-    };
-    
+            self(self, dbegin_left, sbegin_left, sz_left);
+            self(self, dbegin_right, sbegin_right, sz_right);
+            merge(src, dbegin_left, dend_left, dbegin_right, dend_right);
+        };
+
     std::vector<std::ranges::range_value_t<RNG>> dst{rng};
     auto src_itr = std::begin(rng);
     auto dst_itr = std::begin(dst);
 
-    top_down_split_merge(src_itr, dst_itr, static_cast<long>(std::size(rng)));
-
+    // NOTE: (A.1) The precondition on the size of the range rng depends
+    // on the location in memory of the start dst. If the address of the
+    // begining of dst memory storage is X, then X + size(rng) should be
+    // less than or equal to the maximum addressible limit of the system.
+    top_down_split_merge(top_down_split_merge,
+        src_itr,
+        dst_itr,
+        static_cast<std::ranges::range_difference_t<RNG>>(size));
 }
 } // namespace algo
