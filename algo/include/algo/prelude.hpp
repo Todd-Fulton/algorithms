a69f22c12a5cf8ab069626ca78b088f56872bc49
Todd W. Fulton <todd.w.fulton@gmail.com>
2024-02-06 11:23:34 -0600

    add prelude.hpp, prologue.hpp & files to CMake

---
+++ algo/include/algo/prelude.hpp
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2023  Todd W. Fulton
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *published by the Free Software Foundation, either version 3 of the
+ *License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *License along with this program.  If not, see
+ *<https://www.gnu.org/licenses/>.
+ **/
+
+#define FWD(x) std::forward<decltype(x)>(x)
+#define RNG_VALUE_T(range) ranges::range_value_t<decltype(range)>
+#define RNG_ITR_T(range) ranges::iterator_t<decltype(range)>
+
